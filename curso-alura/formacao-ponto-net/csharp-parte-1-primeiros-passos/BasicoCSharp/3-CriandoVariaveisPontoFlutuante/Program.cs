a49cf9376f0278faac25da744593816e670c9465
leandroIFarias <leandro.i.farias@avanade.com>
2021-04-10 09:22:36 -0300

    criacao do codigo do projeto "CriandoVariaveisPontoFlutuante"

--- curso-alura/formacao-ponto-net/csharp-parte-1-primeiros-passos/BasicoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
+++ curso-alura/formacao-ponto-net/csharp-parte-1-primeiros-passos/BasicoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -6,7 +6,25 @@ namespace _3_CriandoVariaveisPontoFlutuante
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Executando projeto 3 Criando variaveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15 / 2.0;
+            Console.WriteLine(idade);
+
+            idade = 5 / 3;
+            Console.WriteLine("5 / 3 = " + idade);
+
+            idade = 5.0 / 3;
+            Console.WriteLine("5.0 / 3 = " + idade);
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair . . . ");
+            Console.ReadLine();
         }
     }
 }
