d4586627c82b6287ff095323cb656aaf0013e61c
leandro.i.farias <leandro.i.farias@avanade.com>
2021-04-10 09:10:55 -0300

    Remocao e Criacao dos 3 primeiros projetos

---
+++ curso-alura/formacao-ponto-net/csharp-parte-1-primeiros-passos/BasicoCSharp/02-CriandoVariaveis/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine("Sua idade é " + idade + "!");
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair. . . ");
+            Console.ReadLine();
+        }
+    }
+}
