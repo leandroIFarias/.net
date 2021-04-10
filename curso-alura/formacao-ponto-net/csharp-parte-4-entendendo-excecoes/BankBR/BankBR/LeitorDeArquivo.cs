41bf4fdda0ad0b915b117ff24acb570bc9bd7325
leandroIFarias <leandro.i.farias@avanade.com>
2021-04-10 12:58:00 -0300

    Projeto tratamento de excecao

---
+++ curso-alura/formacao-ponto-net/csharp-parte-4-entendendo-excecoes/BankBR/BankBR/LeitorDeArquivo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BankBR
+{
+    public class LeitorDeArquivo : IDisposable
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+
+            // throw new FileNotFoundException();
+
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha. . .");
+
+            throw new IOException();
+
+            return "Linha do arquivo";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+
+}
